ded0aefe1ec13786d5781439e36fed621542c9e6
lehuy0311 <lehuy0311@gmail.com>
2023-03-08 21:02:20 +0700

    ss16

---
+++ module2/module2/src/giai_thuat/AddSpace.java
@@ -0,0 +1,12 @@
+package giai_thuat;
+
+import java.util.Scanner;
+
+public class AddSpace {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhập vào chuỗi cần kiểm tra");
+        String input = scanner.nextLine();
+
+    }
+}
