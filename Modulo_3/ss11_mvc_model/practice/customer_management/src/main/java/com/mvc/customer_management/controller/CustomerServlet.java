88fa665453b63943f5b5a099ba5422137d6a8c96
lehuy0311 <lehuy0311@gmail.com>
2023-05-24 16:21:37 +0700

    ss11

---
+++ Modulo_3/ss11_mvc_model/practice/customer_management/src/main/java/com/mvc/customer_management/controller/CustomerServlet.java
@@ -0,0 +1,4 @@
+package com.mvc.customer_management.controller;
+
+public class CustomerServlet {
+}
---
+++ Modulo_3/ss11_mvc_model/practice/customer_management/src/main/java/com/mvc/customer_management/service/CustomerService.java
@@ -0,0 +1,15 @@
+package com.mvc.customer_management.service;
+
+import com.mvc.customer_management.model.Customer;
+
+import java.util.List;
+
+public interface CustomerService {
+    List<Customer> findAll();
+    void save(Customer customer);
+    Customer findById(int id);
+    void update(int id, Customer customer);
+    void remove(int id);
+
+
+}
